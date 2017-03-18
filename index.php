9b45cca5b7ce5ba9aef8fd6a39d27fd03bb38055
Sergio N. Martín <senumar@MacBook-Pro-de-Sergio.local>
2017-03-18 23:09:34 +0100

    Index

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+		
+		Hola que tal
+	</title>
+</head>
+<body>
+
+<?php
+
+$nombre= "Sergio";
+	
+
+	$nombre= "Sergio";
+	echo "Hola ".$nombre;
+?>
+
+<form action="index.php" method="post">
+	<input type="text" name="nombre" placeholder="escribe tu nombre">
+	<br>
+	<input type="Submit" value="enviar">
+
+</body>
+</html>
